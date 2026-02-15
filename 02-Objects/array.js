4fb841abb07a4f5a6d05d7713c19f847883880f9
Raunak Sharma <raunaksh75@gmail.com>
2026-02-15 22:00:42 +0530

    Add files via upload

---
+++ 02-Objects/array.js
@@ -0,0 +1,21 @@
+//------------array----------------
+// copying array always creates shallow copy (have same underlying value) rather than deep copies (different values)
+// let myarr=[33,42,2,244,4]
+// console.log(myarr[0]);
+
+// const marr=new Array(3,42,42,4,6)
+// console.log(marr);
+
+// marr.push(5) // append
+// console.log(marr);
+
+// marr.pop() // removes last element
+// console.log(marr);
+
+// console.log(marr.includes(42)); // confirms if the given element is
+
+const myn1=new Array(4,13,6,8,2)
+const myn2=new Array(7,5,2,4,6)
+console.log(myn1.slice(1,3)); // does not include 3rd indexing
+console.log(myn2.splice(1,3)); // does include 3rd indexing
+
