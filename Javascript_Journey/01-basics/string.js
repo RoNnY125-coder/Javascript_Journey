69caf229c30418c87cc7676556f611fcbdf7f7b8
Raunak Sharma <raunaksh75@gmail.com>
2026-02-17 16:44:35 +0530

    Reorganize string concatenation examples in string.js

--- Javascript_Journey/01-basics/string.js
+++ Javascript_Journey/01-basics/string.js
@@ -2,12 +2,13 @@ const name = "Ronny"
 const value = 10000
 //to concatenate string
 //old way
-// console.log(name+value+"dih")
+console.log(name+value+"dih")
 //new way
-// console.log(`Hi my name is ${name} and my value is ${value}`);  //better way
+console.log(`Hi my name is ${name} and my value is ${value}`);  //better way
 
 const gname = new String('God of War')
 const gname2 = String('God of War')
 console.log(gname)
 console.log(gname2)
+
 console.log(gname[0]) 
