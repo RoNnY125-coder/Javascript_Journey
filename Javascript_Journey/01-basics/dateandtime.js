7548005b336e68c75527747123b672449432271b
Raunak Sharma <raunaksh75@gmail.com>
2026-02-17 16:42:56 +0530

    Restore and update date and time examples

--- Javascript_Journey/01-basics/dateandtime.js
+++ Javascript_Journey/01-basics/dateandtime.js
@@ -1,14 +1,14 @@
 // ------------dates--------
-// const mydate= new Date()
-// console.log(mydate.toDateString()); // stores date
-// console.log(mydate.toISOString()); // stores in iso 
-// console.log(mydate.toJSON()); // stores in json
-// console.log(mydate.toLocaleString()); // stores like we mention date in notebook
-// console.log(typeof mydate) // it is an object
+const mydate= new Date()
+console.log(mydate.toDateString()); // stores date
+console.log(mydate.toISOString()); // stores in iso 
+console.log(mydate.toJSON()); // stores in json
+console.log(mydate.toLocaleString()); // stores like we mention date in notebook
+console.log(typeof mydate); // it is an object
 
-// let newdate= new Date (2026,1,14) // date in js starts with 0
+let newdate= new Date (2026,1,14) // date in js starts with 0
 let newdate= new Date ("01-15-2026") // if string input , month starts with 1
-// console.log(newdate.toDateString());
+console.log(newdate.toDateString());
 
 //----------Time----------
 let mytime=Date.now()
@@ -21,5 +21,6 @@ console.log(Math.floor(Date.now()/1000)); //convert into seconds
 //For more customization
 newdate.toLocaleString('default',{
     weekday: "long"
+
 
 })  //we can also customize more 
