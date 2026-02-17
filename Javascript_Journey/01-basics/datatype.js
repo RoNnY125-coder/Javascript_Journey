caf80a98081f1dff5cfd500beabd5c608f9d6eea
Raunak Sharma <raunaksh75@gmail.com>
2026-02-17 16:42:02 +0530

    Refactor datatype.js for clarity and readability

--- Javascript_Journey/01-basics/datatype.js
+++ Javascript_Journey/01-basics/datatype.js
@@ -1,5 +1,5 @@
 "use strict"; // for newer version of javascript
-//alert(3+3) // wont run because this is not browser , its node js
+alert(3+3) // wont run because this is not browser , its node js
 //code should be readable
 
 //data types:-
@@ -16,16 +16,16 @@
 // console.log(typeof undefined); // it shows correct output
 
 //--------data type conversion---------
-// let score = "33"
-// console.log(typeof score) // it shows the datatype all in lowercase
+let score = "33"
+console.log(typeof score) // it shows the datatype all in lowercase
 
-// let newvalue=Number(score) // when changing the datatype , it should start capital 
-// console.log(typeof newvalue)
+let newvalue=Number(score) // when changing the datatype , it should start capital 
+console.log(typeof newvalue)
 
-// let score2="33abc"
-// let new2value=Number(score2)
-// console.log(new2value) // it shows NaN which means not a number
-// console.log(typeof new2value) // but it does convert the string to number
+let score2="33abc"
+let new2value=Number(score2)
+console.log(new2value) // it shows NaN which means not a number
+console.log(typeof new2value) // but it does convert the string to number
 // in boolean 
 // empty string = false
 // filled string = true
@@ -33,25 +33,26 @@
 // 0 = false
 
 // ------------operations------------
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**2);
-// console.log(2%3);
-// console.log(2/3);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%3);
+console.log(2/3);
 
-// console.log("1"+3+4); //if the initial part is string , all after this will be considered as string
-// console.log(3+2+"1"); //since the string part is after integer , the integer operation will happen first 
+console.log("1"+3+4); //if the initial part is string , all after this will be considered as string
+console.log(3+2+"1"); //since the string part is after integer , the integer operation will happen first 
 
 //postfix
-// let x=3
-// const y=x++
-// console.log(x);
-// console.log(y);
+let x=3
+const y=x++
+console.log(x);
+console.log(y);
 
 //prefix
-// let x=3
-// const y=++x
-// console.log(x);
-// console.log(y);
+let x=3
+const y=++x
+console.log(x);
+console.log(y);
+
 
