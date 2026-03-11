22a6ece97b24b7ae8de6889473f04ead48ce27b9
Raunak Sharma <raunaksh75@gmail.com>
2026-03-11 11:06:23 +0530

    Restore and update High Order Array Loops examples

--- 05-Iterations/HOA_loops.js
+++ 05-Iterations/HOA_loops.js
@@ -1,32 +1,32 @@
 // High Order Array Loops
 //for of
 
-// const arr=[1,2,3,4,5]
-// for (const i of arr) {
-//     console.log(i);
-// }
+const arr=[1,2,3,4,5]
+for (const i of arr) {
+    console.log(i);
+}
 
-// const greeting="Hello Traveler , I have been expecting you!"
-// for (const greet of greeting) {
-//     if (greet==" "){
-//         console.log("Space detected")
-//         continue
-//     }
-//     console.log(`Each car is ${greet}`);
+const greeting="Hello Traveler , I have been expecting you!"
+for (const greet of greeting) {
+    if (greet==" "){
+        console.log("Space detected")
+        continue
+    }
+    console.log(`Each car is ${greet}`);
     
-// }
+}
 
 // Maps
-// const map = new Map()
-// map.set('IN','India')
-// map.set('USA','United States Of America')
-// map.set('SP','Spain')
-// console.log(map);  // shows the no. of output and the key value pair
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United States Of America')
+map.set('SP','Spain')
+console.log(map);  // shows the no. of output and the key value pair
 // //only unique values , no duplicacy , ordered pairs
 
-// for (const [key,value] of map) {
-//     console.log(key,'->',value);    
-// }
+for (const [key,value] of map) {
+    console.log(key,'->',value);    
+}
 
 const game={
     'game1':'GOW',
@@ -43,24 +43,25 @@ const meraobj={
 
 //for in loop
 
-// for (const key in meraobj) {
-//     console.log(key)
+for (const key in meraobj) {
+    console.log(key)
 
 // }
 // array also have keys , it is by default its indexing positon
 
-// const weopons = ['sword','knife','dagger','bazooka']
-// for (const key in weopons) {
-//     console.log(weopons[key]);
-// }
+const weopons = ['sword','knife','dagger','bazooka']
+for (const key in weopons) {
+    console.log(weopons[key]);
+}
 
 
 
-// const map = new Map()
-// map.set('IN','India')
-// map.set('USA','United States Of America')
-// map.set('SP','Spain')
-// for (const key in map) {
-//     console.log(map[key]);
-// }
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United States Of America')
+map.set('SP','Spain')
+for (const key in map) {
+    console.log(map[key]);
+}
 //not iterable
+
