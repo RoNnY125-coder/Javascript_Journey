1534f48753dcbe30f82dc07ad6033cbac48b5634
Raunak Sharma <raunaksh75@gmail.com>
2026-03-11 23:12:36 +0530

    Enhance for_eachloop.js with forEach examples
    Added examples of forEach loop usage with normal and arrow functions, and demonstrated how to return values from forEach.

--- 05-Iterations/for_eachloop.js
+++ 05-Iterations/for_eachloop.js
@@ -1,26 +1,26 @@
 //for each loop is very important 
 
 //normal function
-// const code=['js','cpp','rust','ruby','ptyhon']
-// code.forEach( function (item){
-//     console.log(item);
-// } 
+const code=['js','cpp','rust','ruby','ptyhon']
+code.forEach( function (item){
+    console.log(item);
+} 
 
 //arrow function
-// code.forEach( (item) => {
-//     console.log(item);
-// })
+code.forEach( (item) => {
+    console.log(item);
+})
 
 //we can also declare a funtion
-// function printME(item) {
-//     console.log(item);
-// }
+function printME(item) {
+    console.log(item);
+}
 
 // code.forEach(printME)
 
-// code.forEach((item,index,arr)=>{
-//     console.log(item,index,arr);
-// })
+code.forEach((item,index,arr)=>{
+    console.log(item,index,arr);
+})
 
 
 //very common operation
@@ -43,4 +43,13 @@ coder.forEach((item)=>{
     console.log(item.lang);
     console.log(item.langfile);
 
+
 })
+
+const code=['js','cpp','rust','ruby','python']
+const value=code.forEach((item)=>{
+    console.log(item);
+    return item
+    
+})
+console.log(value); 
