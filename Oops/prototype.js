245d09bbda3a28862a83dbe290c9fd39d3665147
Raunak Sharma <raunaksh75@gmail.com>
2026-05-08 12:01:46 +0530

    Oops in js

---
+++ Oops/prototype.js
@@ -0,0 +1,14 @@
+// let myname="Raunak"
+// console.log(myname.truelength());
+
+let myhero=["thor","Batman"]
+let heropower={
+    thor: "Hammer",
+    Batman: "bats",
+
+    getthorpower:function(){
+        console.log(`Spidy power is ${this.thor}`)
+    }
+}
+
+heropower.ronny()
